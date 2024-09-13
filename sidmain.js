5f2821cf819c46916611fda3f74e37f130c23605
simozzer <simonmoscrop@gmail.com>
2024-09-13 21:15:46 +0100

    styling changes

--- sidmain.js
+++ sidmain.js
@@ -73,6 +73,14 @@ doMenuChanged = (oEv) => {
 function doPuzzleSolved(info) {
     setInfoText(info);
     document.getElementById("menu").disabled = false;
+    let a = document.getElementsByClassName("suggested");
+    [].forEach.call(a,(el) => {
+        try {
+            el.classList.remove("suggested");
+        } catch {
+
+        }
+    })
 }
 
 const shinningStarData = [0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 3, 0, 0, 0, 0, 4, 0, 0, 5, 0, 0, 6, 0, 7, 0, 0, 0, 0, 0, 8, 0, 0, 0, 7, 0, 0, 0, 7, 0, 0, 3, 8, 0, 0, 9, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 6, 0, 8, 0, 2, 0, 0, 0, 4, 0, 6, 0, 0, 0, 0, 7, 2, 0, 0, 0, 0, 9, 0, 6, 0];
