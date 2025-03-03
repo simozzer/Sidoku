57dd0ccb2673f822bba329deec9ef19cea5564d6
simozzer <SimonMoscrop@hotmail.com>
2025-03-03 03:04:24 +0000

    Added test for Escargot AI, which had claimed to be the world's most difficult sudoko (it isn't)

--- readme.md
+++ readme.md
@@ -14,6 +14,8 @@ It's all about the SPEED: A 24000% speed improvement over my 1st attempt at solv
 The hardest example used to take over 2 minutes on my local machine. I've now got that down to less than 0.5 seconds.
 
 
+Just added 'Escargot AI' as a puzzle, which some on the internet claim is the world's most difficult puzzle. Just like most things on the internet this is a lie.  The most difficult I've found so far is 'Shining Mirror'.
+
 Run with: 
 
 npx @tailwindcss/cli -i ./css/siduko.css -o ./css/output.css --watch
--- src/sidmain.js
+++ src/sidmain.js
@@ -61,6 +61,14 @@ doSolvePressed = (oEv) => {
 doMenuChanged = (oEv) => {
     btn = document.getElementById("solvebutton");
     switch (oEv.target.value) {
+        case "Monster":
+            btn.hidden = true;
+            btn.disabled = true;
+    
+            oEv.target.disabled = true;
+            setupPuzzle(escargotAiData);
+            break;            
+
         case "Difficult":
             btn.hidden = true;
             btn.disabled = true;
@@ -114,8 +122,8 @@ function doPuzzleSolved(info) {
 const shinningStarData = [0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 3, 0, 0, 0, 0, 4, 0, 0, 5, 0, 0, 6, 0, 7, 0, 0, 0, 0, 0, 8, 0, 0, 0, 7, 0, 0, 0, 7, 0, 0, 3, 8, 0, 0, 9, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 6, 0, 8, 0, 2, 0, 0, 0, 4, 0, 6, 0, 0, 0, 0, 7, 2, 0, 0, 0, 0, 9, 0, 6, 0];
 const evilPuzzleData = [9, 0, 0, 0, 4, 3, 1, 6, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 9, 0, 8, 0, 0, 0, 1, 9, 3, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 6, 0, 0, 0, 0, 7, 0, 6, 4, 0, 0, 3, 4, 0, 0, 2, 0, 0, 0, 0, 0];
 const hardPuzzleData = [1,0,0,6,0,5,0,0,9,0,0,0,0,0,0,0,0,0,0,5,3,0,1,0,8,4,0,0,0,0,9,5,1,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,8,0,6,0,0,6,0,7,0,0,0,9,0,5,3,0,0,8,0,7,0,0,2,0,0,9,0,0,0,1,0,0];
-
-
+const escargotAiData = [1,0,0,0,0,7,0,9,0,0,3,0,0,2,0,0,0,8,0,0,9,6,0,0,5,0,0,0,0,5,3,0,0,9,0,0,0,1,0,0,8,0,0,0,2,6,0,0,0,0,4,0,0,0,3,0,0,0,0,0,0,1,0,0,4,0,0,0,0,0,0,7,0,0,7,0,0,0,3,0,0];
+                       
 
 class SidukoPuzzle {
     #rowCells = [];
