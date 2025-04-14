e0779cf3f829956b921f0d47f6cefe99d3921486
simozzer <SimonMoscrop@hotmail.com>
2025-04-14 22:16:02 +0100

    configure volume for each sound

--- src/sidsounds.js
+++ src/sidsounds.js
@@ -3,20 +3,20 @@ class SidukoSounds {
     #cachedSounds
     constructor() {
         this.#cachedSounds = {};
-        this.#getSound('click1');
-        this.#getSound('hiss1');
-        this.#getSound('all systems go');
+        this.#getSound('click1',1);
+        this.#getSound('hiss1',0.6);
+        this.#getSound('all systems go',0.2);
     }
 
     playSound(sSoundName) {
         this.#getSound(sSoundName).play();
     }
 
-    #getSound(sSoundName) {
+    #getSound(sSoundName, volume) {
         let oSound = this.#cachedSounds[sSoundName];
         if (!oSound) {
             oSound = new Audio(`./resources/sounds/${sSoundName}.wav`);
-            oSound.volume = 0.2;
+            oSound.volume = volume;
             this.#cachedSounds[sSoundName] = oSound;
         }
         return oSound;
