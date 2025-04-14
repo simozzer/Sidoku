9a006985c490e994012d56129b6d1899a7e59dda
simozzer <SimonMoscrop@hotmail.com>
2025-04-14 22:48:56 +0100

    further sound fix

--- src/sidsounds.js
+++ src/sidsounds.js
@@ -3,9 +3,10 @@ class SidukoSounds {
     #cachedSounds
     constructor() {
         this.#cachedSounds = [];
-        this.#getSound('Click1',1);
-        this.#getSound('hiss1',0.6);
+        this.#getSound('Click1',0.8);
+        this.#getSound('hiss1',0.4);
         this.#getSound('all systems go',0.2);
+        this.#getSound('swipe_9',0.2);
     }
 
     playSound(sSoundName) {
@@ -13,7 +14,7 @@ class SidukoSounds {
     }
 
     #getSound(sSoundName, volume) {
-        let oSound = this.#cachedSounds.find(s => s.name === sSoundName);
+        let oSound = this.#cachedSounds.find(s => s.name.toLowerCase() === sSoundName.toLowerCase());
         if (!oSound) {
             const oAudio =  new Audio(`./resources/sounds/${sSoundName}.wav`);
             oAudio.volume = volume;
