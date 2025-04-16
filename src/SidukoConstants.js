f62bed1ba9faf11f78ef99921bcba8b6cf2c26d9
simozzer <SimonMoscrop@hotmail.com>
2025-04-16 04:24:43 +0100

    adjusted start values

--- src/SidukoConstants.js
+++ src/SidukoConstants.js
@@ -2,13 +2,13 @@ class SidukoConstants {
   static HINT_BUY_BOOST_TURNS = 5;
   static INITIAL_SEEKER_LIVES = 1;
 
-  static BOOST_UP_LEVEl_COST = 2;
-  static BOOST_LIFE_COST = 1;
+  static BOOST_UP_LEVEl_COST = 5;
+  static BOOST_LIFE_COST = 2;
 
   static DEFAULT_FUNDS = 6;
 
   static GAME_DURATION_SECONDS = 540;
   static TIME_BOOST_SECONDS = 60;
 
-  static GUESSES_MULTIPLER = 0.1;// 1.3;
+  static GUESSES_MULTIPLER = 1.3;
 }
