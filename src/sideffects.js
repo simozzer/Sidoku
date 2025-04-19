637eb4a52a63f8adf02fd746656f1fa841409fd0
simozzer <SimonMoscrop@hotmail.com>
2025-04-19 04:25:12 +0100

    some more rotation

--- src/sideffects.js
+++ src/sideffects.js
@@ -75,10 +75,9 @@ class SidukoElementEffects {
 
   static async explodeAllCells() {
     const allCells = Array.from(document.querySelectorAll(".sidukoTable table td"));
-
     for(let i=0; i < allCells.length; i++) {
       SidukoElementEffects.slideCellOut(allCells[i]);
-      await new Promise(resolve => setTimeout(resolve, 20));  // simulate async operation
+      await new Promise(resolve => setTimeout(resolve, 50));  // simulate async operation
       allCells[i].style.backgroundColor = "white";  // reset cell background color after animation
       allCells[i].style.color = "black";  // reset cell text color after animation
     }
